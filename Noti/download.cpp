95eb675593d91a32be47d56d457506a95e6b594e
Force Charlie <charlieio@outlook.com>
2018-05-01 22:03:19 +0800

    添加项目文件。

---
+++ Noti/download.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web;
+using namespace Windows::Web::Http;
+using namespace Windows::Web::Http::Filters;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Streams;
+
+
+/*
+HttpBaseProtocolFilter support HTTP2 by default.
+*/
+IAsyncAction Notidownload(const wchar_t *url,const wchar_t *savefile){
+	try{
+		if(savefile==nullptr){
+			////
+		}
+		printf("download url: %ls %ls\n", url,savefile);
+		HttpBaseProtocolFilter baseFilter;
+		baseFilter.AllowAutoRedirect(true);
+		//printf("HTTP version: %d\n",baseFilter.MaxVersion()); default enable HTTP2
+		HttpClient client(baseFilter);
+		client.DefaultRequestHeaders().Append(L"User-Agent", L"dolt/1.0");
+		Uri uri(url);
+		printf("request...  %ls\n",client.DefaultRequestHeaders().UserAgent().ToString().c_str());
+		auto resp=co_await client.GetAsync(uri);
+		resp.EnsureSuccessStatusCode();
+		if(resp.Headers().HasKey(L"Content-Disposition")){
+			auto disp=resp.Headers().Lookup(L"Content-Disposition");
+			printf("Content-Disposition: %ls\n", disp.c_str());
+		}
+		auto body = co_await resp.Content().ReadAsStringAsync();
+		printf("%ls\n", body.c_str());
+	 }catch(std::exception &e){
+		printf("download url: %s\n", e.what());
+	 }
+}
---
+++ Noti/pch.h
@@ -0,0 +1,13 @@
+﻿//
+// pch.h
+// Precompiled header for commonly included header files
+//
+
+#pragma once
+
+#include <winrt/Windows.Foundation.h>
+//#include <winrt/Windows.Networking.h>
+#include <winrt/Windows.Web.Http.h>
+#include <winrt/Windows.Web.Http.Headers.h>
+#include <winrt/Windows.Web.Http.Filters.h>
+#include <winrt/Windows.Storage.Streams.h>
